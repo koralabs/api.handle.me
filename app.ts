dede5f0191b6ec70df0824e14169c8cd8eb20583
papag00se <jlanderson@gmail.com>
2026-01-13 14:04:44 -0800

    includes instead of ordinal

--- app.ts
+++ app.ts
@@ -143,7 +143,7 @@ class App {
             await handlesRepo.initialize();
             
             // If we're running local we want the scanner to replace ogmios scanning
-            if (process.argv[2] === '--with-scanner') {
+            if (process.argv.includes('--with-scanner')) {
                 await (async () => {
                     const lambda = await import('./lambdas/scanner');
                     setInterval(lambda.lambdaHandler, 60000)
